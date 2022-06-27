22c36eb11ce520e0afe0b76471739261ca5d5e94
reskybayu234 <75589544+reskybayu234@users.noreply.github.com>
2022-06-27 22:00:13 +0700

    vuetify

---
+++ src/components/NavbarTodo.vue
@@ -0,0 +1,53 @@
+<template>
+  <nav>
+    <v-toolbar flat app>
+      <v-toolbar-title class="text-uppercase grey--text">
+        <v-btn
+          class="btn1"
+          @click="drawer = !drawer"
+          fab
+          small
+          depressed
+          color="grey"
+        >
+          <v-icon>mdi-circle-double</v-icon>
+        </v-btn>
+        <span class="font-weight-light">Todo</span>
+        <span>Ninja</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn depressed>
+        <span>Sign Out</span>
+        <!-- <font-awesome-icon icon="fa-alicorn" /> -->
+        <!-- <v-icon right>fas fa-right-from-bracket</v-icon> -->
+        <v-icon right>mdi-logout</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="mini"
+      absolute
+      dark
+      temporary
+    >
+    </v-navigation-drawer>
+  </nav>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      drawer: true,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.btn1 {
+  min-width: 10px;
+  height: 20px;
+  width: 20px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <v-btn class="pink white--text">Click Me</v-btn>
+    <v-btn depressed elevation="5" color="pink">Click me</v-btn>
+    <v-btn flat color="pink">Click me</v-btn>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  components: {},
+};
 </script>
